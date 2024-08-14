a27c006621d29b3f2ffcd4ae51b5743cb3984a2b
geethpriya-v <geethpriya.er@gmail.com>
2024-08-14 13:10:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,12 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     
-    ingredients_string =''
+      ingredients_string =''
     
-    for fruit_chosen in ingredients_list:
-        ingredients_string+= fruit_chosen+ ''
+      for fruit_chosen in ingredients_list:
+          ingredients_string+= fruit_chosen+ ''
+          fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+          fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
 
     #st.write(ingredients_string)
     
@@ -43,7 +45,3 @@ if ingredients_list:
         
        session.sql(my_insert_stmt).collect()
        st.success('Your Smoothie is ordered!', icon="✅")
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
